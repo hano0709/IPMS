b34f622aba053a816d9b6fc9892e12b813bf5ac3
Hano Varghese/Marvel IT/Intern/General <Hano.Varghese@BajajGeneral.com>
2026-08-07 11:57:09 +0530

    init commit

---
+++ src/main/resources/db/migration/V2__testing.sql
@@ -0,0 +1,1 @@
+ALTER TABLE customers ADD column customer_number VARCHAR(50);
---
+++ src/test/java/com/bajaj/IPMS/IpmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bajaj.IPMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
