9d16caa58188e62c9982feed6891896df297db0f
Hano.Varghese <hanojoby@gmail.com>
2026-09-23 18:22:47 +0530

    smoke test disabled

--- src/test/java/com/bajaj/IPMS/IpmsApplicationTests.java
+++ src/test/java/com/bajaj/IPMS/IpmsApplicationTests.java
@@ -1,9 +1,11 @@
 package com.bajaj.IPMS;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Disabled smoke test in CI")
 class IpmsApplicationTests {
 
 	@Test
